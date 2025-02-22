f1e894872662d59c1a839ee4f7a4afbab9ebc307
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-22 23:34:18 +0530

    added update one course controller

--- 23_bulidApi/main.go
+++ 23_bulidApi/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"math/rand"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -22,12 +25,13 @@ type Author struct {
 }
 
 // fake DB
-var courses = []Course
+var courses = []Course{}
 
 // middleware or helper
 
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 func main() {
 	r := mux.NewRouter()
@@ -49,7 +53,7 @@ func getAllCourses(res http.ResponseWriter, req *http.Response) {
 
 }
 
-func getOnecourse(res http.ResponseWriter, req *http.Request) {
+func getOneCourse(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("get one course")
 	res.Header().Set("content-Type", "application/json")
 
@@ -66,4 +70,61 @@ func getOnecourse(res http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(res).Encode("No Course found with given id")
 	return
 
+}
+
+func createOneCourse(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Create one course")
+	res.Header().Set("content-Type", "application/json")
+
+	//what if : body is empty
+	if req.Body == nil {
+		json.NewEncoder(res).Encode("Please send some data")
+
+	}
+	//what about if:{}
+	var course Course
+	_ = json.NewDecoder(req.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(res).Encode("No data inside JSON")
+
+		return
+	}
+
+	//generate unique id, string
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId=strconv.Itoa(rand.Intn(100))
+	courses=append(courses,course)
+	json.NewEncoder(res).Encode(course)
+	return
+
+}
+
+func updateOneCourse(res http.ResponseWriter, req *http.Request){
+	fmt.Println("update on course")
+	res.Header().Set("content-Type", "application/json")
+
+	//first: grab id from req
+	params:=mux.Vars(req)
+
+	//loop ,id,remove,add with my id
+    for index,course:=range courses{
+		if course.CourseId==params["id"]{
+			courses=append(courses[:index],courses[index+1:]... )
+			var course Course
+			_=json.NewDecoder(req.Body).Decode(&course)
+			course.CourseId=params["id"]
+			courses=append(courses, course)
+			json.NewEncoder(res).Encode(course)
+			return
+
+
+		}
+
+	}
+
+
+}
+
+func deleteOneCourse(res http.ResponseWriter,req *http.Request){
+	
 }
