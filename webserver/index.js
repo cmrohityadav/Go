9ec9be2bd57b1bba2762a3d706cafb56a8d4a3df
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-22 16:03:23 +0530

    added apijson for testing json handling in go

--- webserver/index.js
+++ webserver/index.js
@@ -24,6 +24,23 @@ app.post('/post', (req, res) => {
 app.post('/postform', (req, res) => {
     res.status(200).send(JSON.stringify(req.body));
 })
+app.get('/apijson',(req,res)=>{
+  let arrayData = [
+    { name: "Amit", class: 1, doy: 2015 },
+    { name: "Sneha", class: 2, doy: 2013 },
+    { name: "Rahul", class: 3, doy: 2017 },
+    { name: "Priya", class: 4, doy: 2016 },
+    { name: "Vikram", class: 6, doy: 2014 },
+    { name: "Kiran", class: 2, doy: 2018 },
+    { name: "Neha", class: 1, doy: 2019 },
+    { name: "Sandeep", class: 8, doy: 2012 },
+    { name: "Meera", class: 9, doy: 2011 },
+    { name: "Arjun", class: 10, doy: 2010 }
+  ];
+
+
+return res.status(200).json({message:"success",data:arrayData})
+})
   
 
 app.listen(port, () => {
