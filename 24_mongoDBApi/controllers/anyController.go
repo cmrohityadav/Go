8dded22431b8690786bfb547d30a17fb2da6c30f
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-26 23:16:05 +0530

    added insert and update controller

---
+++ 24_mongoDBApi/controllers/anyController.go
@@ -0,0 +1,77 @@
+package anyController
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	anymodel "gihub.com/cmrohityadav/go/mongodbapi/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+const connectionString = "mongodb://localhost:27017"
+const dbName = "firstDbForGo"
+const collectionName = "watchList"
+
+var collection *mongo.Collection
+
+func init() {
+	// Define MongoDB client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+
+	// Ping the database to ensure a successful connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not ping MongoDB:", err)
+	}
+
+	fmt.Println("MongoDB connection successful")
+
+	// Get the collection reference
+	collection = client.Database(dbName).Collection(collectionName)
+	fmt.Println("Collection instance is ready")
+}
+
+// MongoDB helpers - file
+
+//insert 1 record
+
+func insertOneMovie(movie anymodel.Netflix){
+	inserted,err:=collection.InsertOne(context.Background(),movie)
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in  db with id: ",inserted.InsertedID)
+
+}
+
+//update 1 record
+
+func updateOneMovie(movieId string){
+	id,_:=primitive.ObjectIDFromHex(movieId)
+	filter:=bson.M{"_id":id}
+	update:=bson.M{"$set":bson.M{"watched":true}}
+	result,err:=collection.UpdateOne(context.Background(),filter,update)
+	if err !=nil{
+		log.Fatal(err)
+	}
+	fmt.Println("modified count : ",result.ModifiedCount)
+}
+
+
+
+
+
