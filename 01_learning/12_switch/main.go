e0b42cda8cc04f49b30efc05950485e7d177d316
Rohit Yadav <cmrohityadav23@gmail.com>
2025-09-20 19:42:23 +0530

    added switch

--- 01_learning/12_switch/main.go
+++ 01_learning/12_switch/main.go
@@ -1,40 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	fmt.Println("Welcome to Conditions: if else")
 
-	var iLoginCount int = 10
-	var sMessage string
+func main(){
 
-	if iLoginCount < 10 {
-		sMessage = "Regular User"
-	} else if iLoginCount > 10 {
-		sMessage = "Watch Out"
-	} else {
-		sMessage = "Exactly 10 login count"
+	day:=7;
+
+	switch day {
+	case 1:
+	fmt.Println("Its Mon");
+
+	case 2:
+		fmt.Println("Its Tue");
+	
+	case 3:
+		fmt.Println("Its Wed");
+	
+	case 4:
+		fmt.Println("Its Thu");
+	
+	case 5:
+		fmt.Println("Its Fri");
+	
+	case 6:
+		fmt.Println("Its Sat");
+	
+	case 7:
+		fmt.Println("Its Sun");
+
+	default:
+		fmt.Println("Incorrect option");
 	}
 
-	fmt.Println(sMessage)
 
-	fmt.Println("\n\nSomething new in Go")
-	if 9%2 == 0 {
-		fmt.Println("Even")
-	} else {
-		fmt.Println("Odd")
+	num:=2;
+
+	switch num {
+	case 1:
+		fmt.Println("One");
+	case 2:
+		fmt.Println("two");
+		fallthrough
+	case 3:
+		fmt.Println("Three");
+	case 4:
+		fmt.Println("four");
+	default:
+		fmt.Println("Default");
 	}
 
-	fmt.Println("\n\nIMP for Web Request Handling")
 
-	if iNum := 3; iNum < 10 {
-		fmt.Println("Num is less than 10")
-	} else {
-		fmt.Println("Num is greater than or equal to 10")
+	// type switch
+
+	myType:=func (i interface{}){
+		switch typeOf:=i.(type){
+		case int:
+			fmt.Println("Int");
+		case string:
+			fmt.Println("String");
+		case bool:
+			fmt.Println("Bool");
+		default:
+			fmt.Println("Other",typeOf);
+		}
 	}
 
-	// Example usage in error handling
-	// if err != nil {
-	//     fmt.Println("An error occurred")
-	// }
+	myType(1.1);
+
 }
