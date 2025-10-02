c451fd5e3231e5d71ebd408378cffbefa4fe95e4
Rohit Yadav <cmrohityadav23@gmail.com>
2025-10-02 19:35:08 +0530

    basic http server

--- 02_ProtoGo/01_studentcrudapi/cmd/studentapi/main.go
+++ 02_ProtoGo/01_studentcrudapi/cmd/studentapi/main.go
@@ -1,8 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"main/internal/config"
+	"net/http"
+)
 
 func main(){
-	fmt.Println("Welcome student API");
+fmt.Println("Welcome student API");
+
+// ----------load Config---------
+cfg:=config.MustLoad()
 	
+// ----------Database setup---------
+
+
+// ----------setup router-----------
+router:=http.NewServeMux();
+
+router.HandleFunc("GET /",func (w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Welocome to HOME students api"));
+})
+
+
+
+
+// ----------setup server---------
+server:=http.Server{
+	Addr: cfg.Addr,
+	Handler: router,
+
+}
+
+err:=server.ListenAndServe();
+fmt.Println("Server started");
+if err!=nil{
+	log.Fatal("failed to start server");
+}
+
+
 }
