7116e21eb89b97aa19a4fdc248e38938eab340bf
Rohit Yadav <cmrohityadav23@gmail.com>
2025-10-04 01:26:20 +0530

    added about mutex basic

--- 01_learning/26_goroutines/01_basic.go
+++ 01_learning/26_goroutines/01_basic.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 func task(id int){
 	fmt.Println("Doing task ",id);
 }
+
+var signals=[]string{"test"}
+
 
 
 func main(){
@@ -18,6 +22,61 @@ func main(){
 	time.Sleep(time.Second*2)
 
 // ---------02 code----------------
+
 	go Greeter("hello");
 	Greeter("world");
+
+//--------03 code (Wait group) ----------
+websitesList:=[]string{"https://github.com/cmrohityadav","https://google.com","https://facebook.com",}
+
+var wg sync.WaitGroup //In real project we create pointer of it
+
+
+for _,web:= range websitesList{
+	wg.Add(1)
+	go GetStatusCode(web,&wg);
+}
+wg.Wait();
+
+fmt.Println("signals :",signals);
+
+
+
+//--------Mutex-----------
+
+var coruptCounter int=0;
+
+for i := 0; i < 5000; i++ {
+    go func() {
+       coruptCounter++;
+    }()
+    go func() {
+        coruptCounter++;
+    }()
+}
+
+time.Sleep(2 * time.Second)
+
+fmt.Println("corupt Counter:",coruptCounter);
+
+var mut sync.Mutex //In real project we create pointer of it
+
+var safeCounter int = 0
+
+for i := 0; i < 5000; i++ {
+    go func() {
+        mut.Lock()
+        safeCounter++
+        mut.Unlock()
+    }()
+    go func() {
+        mut.Lock()
+        safeCounter++
+        mut.Unlock()
+    }()
+}
+
+time.Sleep(2 * time.Second)
+fmt.Println("safe Counter:", safeCounter)
+
 }
