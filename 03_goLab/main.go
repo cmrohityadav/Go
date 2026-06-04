bc2b41f778df6e43c12f449a3cc51112c9d988ba
cmrohityadav <cmrohityadav23@gmail.com>
2026-06-04 22:38:51 +0530

    golab:added var

--- 03_goLab/main.go
+++ 03_goLab/main.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
-type Market int
-const (
-	EQ Market=iota+100
-	F
-	O
-	CD
+var location string="kandivali";
+
+var (
+	city string="mumbai"
+	state string="maharashtra"
+	pincode int=400066
 
 )
+
+var fname,mname,lname string="rohit","cm","yadav"
+
+
 func main() {
-	fmt.Println(F)
+
+	fmt.Println("hello rohit");
+
+
+	var name string ="rohit"
+	fmt.Println(name)
+
+	surname:="yadav"
+	fmt.Println(surname)
+
+	fmt.Println(location)
+	fmt.Println("city :",city)
+	fmt.Println("state :",state)
+	fmt.Println("pincode :",pincode)
+	fmt.Println("first name :",fname)
+	fmt.Println("middle name :",mname)
+	fmt.Println("last name:",lname)
+
+
 
 }
