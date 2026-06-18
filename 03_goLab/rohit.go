46fd0d9863c9ace2a4938a51674ae03fe094ce78
cmrohityadav <cmrohityadav23@gmail.com>
2026-06-18 21:39:13 +0530

    golab: slice

--- 03_goLab/rohit.go
+++ 03_goLab/rohit.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func main() {
 
-	var ages[3] int
+	var age []int
 
-	ages[0]=00
-	ages[1]=10
-	ages[2]=20
+	age = append(age, 10)
+	age = append(age, 20)
+	age = append(age, 30)
+	age[0]=1;
+	age[1]=2;
+	age[2]=3;
 
 	for i:=0;i<3;i++{
-		fmt.Println(ages[i])
+		fmt.Println(age[i])
 	}
-
 
 }
