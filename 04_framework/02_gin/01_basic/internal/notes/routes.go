bde75e3c57ab62436d6cdc406be3d58a62b8026e
cmrohityadav <cmrohityadav23@gmail.com>
2026-07-20 23:46:38 +0530

    added routes

---
+++ 04_framework/02_gin/01_basic/internal/notes/routes.go
@@ -0,0 +1,18 @@
+package notes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func RegisterRoutes(r *gin.Engine,pool *pgxpool.Pool){
+	repo:=NewPostgresRepository(pool)
+	service:=NewService(repo)
+	handler:=NewHandler(service)
+
+	notesGroup:=r.Group("/notes")
+	{
+		notesGroup.POST("",handler.Create)
+	}
+	
+}
