f3648952e835dea6f58095d1a0e94ecbb5d359e5
cmrohityadav <cmrohityadav23@gmail.com>
2026-08-06 23:26:45 +0530

    updated NewRouter function

--- 04_framework/02_gin/02/internal/httpserver/router.go
+++ 04_framework/02_gin/02/internal/httpserver/router.go
@@ -1,14 +1,27 @@
 package httpserver
 
-import "github.com/gin-gonic/gin"
+import (
+	"auth/internal/config"
+	"auth/internal/user"
 
-func NewRouter() *gin.Engine{
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewRouter(ptrDbPool *pgxpool.Pool,cfg config.Config) *gin.Engine{
 	r:=gin.New()
 
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	r.GET("/health",health)
+
+	userRepo:=user.NewRepository(ptrDbPool)
+	userService:=user.NewService(userRepo,cfg.JWT_SECRET)
+	userHanlder:=user.NewHandle(userService)
+
+	r.POST("/create",userHanlder.CreateUser)
+
 
 	return r
 }
